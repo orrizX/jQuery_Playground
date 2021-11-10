02a8268b0515abdc99f0b850987bae80c3b29234
orrizX <zo292@nyu.edu>
2021-11-10 16:21:58 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document)
+  .mousemove(function(e) {
+    $('.cursor')
+      .eq(0)
+      .css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    setTimeout(function() {
+      $('.cursor')
+        .eq(1)
+        .css({
+          left: e.pageX,
+          top: e.pageY
+        });
+    }, 100);
+  })
