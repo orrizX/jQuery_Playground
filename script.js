2c03746c85fcf74424acd030ce32577dac21d944
orrizX <zo292@nyu.edu>
2021-11-11 15:28:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,19 @@ $(document)
     });
   });
   // animation
+
+  $(function () {
+    "use strict";
+    
+    $(".popup img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+    
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+    
+  });
+  //image popup
