759d10b4e827489e35fc8bdd4d789c6913de0c35
orrizX <zo292@nyu.edu>
2021-11-11 11:35:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,14 @@ $(document)
     }, 50);
   })
   // cursor
+
+  $(document).ready(function(){
+    $("#runner").click(function(){
+      var div = $("#runner");
+      div.animate({height: '300px', opacity: '0.4'}, "slow");
+      div.animate({width: '800px', opacity: '0.8'}, "slow");
+      div.animate({height: '100px', opacity: '0.4'}, "fast");
+      div.animate({width: '100px', opacity: '0.8'}, "fast");
+    });
+  });
+  // animation
